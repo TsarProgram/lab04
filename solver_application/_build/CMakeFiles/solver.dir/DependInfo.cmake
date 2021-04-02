5e047fc793b00236876c609e8b78b9aa2649a2af
TsarProgram <octorozhnozlaya@gmail.com>
2021-04-02 21:50:51 +0300

    new

---
+++ solver_application/_build/CMakeFiles/solver.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/vitaliy/test/solver_application/equation.cpp" "/home/vitaliy/test/solver_application/_build/CMakeFiles/solver.dir/equation.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/vitaliy/test/solver_lib"
+  "/home/vitaliy/test/formatter_lib"
+  "/home/vitaliy/test/formatter_ex_lib"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
